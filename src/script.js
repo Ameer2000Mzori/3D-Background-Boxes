de6120dad6e1ef3861a8b190b4c5a427203e10e1
Ameer <ameerapex496@gmail.com>
2023-12-11 06:11:27 +0100

    styling fix / added functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-console.log("hallo world");
+var boxesContainer = document.getElementById("boxes");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () { return boxesContainer.classList.toggle("big"); });
+function createBoxes() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var box = document.createElement("div");
+            box.classList.add("box");
+            box.style.backgroundPosition = "".concat(-j * 125, "px ").concat(-i * 125, "px");
+            boxesContainer.appendChild(box);
+        }
+    }
+}
+createBoxes();
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,17 @@
-console.log("hallo world");
+const boxesContainer: any = document.getElementById("boxes");
+const btn: any = document.getElementById("btn");
+
+btn.addEventListener("click", () => boxesContainer.classList.toggle("big"));
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
+      boxesContainer.appendChild(box);
+    }
+  }
+}
+
+createBoxes();
